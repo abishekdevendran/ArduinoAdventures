7f0817f7e1ea7e708db8c40b3d9ea81b9f14c831
Abishek Devendran <abishekdevendran@gmail.com>
2023-03-31 23:37:28 +0530

    Mesh fallback local mosquitto added

--- ArduinoMesh/src/main.cpp
+++ ArduinoMesh/src/main.cpp
@@ -1,27 +1,102 @@
 #include <Arduino.h>
-// #include <SoftwareSerial.h>
+#include <Adafruit_Sensor.h>
+#include <SoftwareSerial.h>
+#define USE_ARDUINO_INTERRUPTS false //set to true if you are using an Arduino Uno, Leonardo, or Micro
+#include <PulseSensorPlayground.h>
+#include <Ultrasonic.h>
+#include <DHT.h>
+#include <ArduinoJson.h>
 
-// SoftwareSerial mySerial(2, 3); // RX, TX
-// void setup() {
-//   // put your setup code here, to run once:
-//   Serial.begin(115200);
-//   // initialize software serial
-//   mySerial.begin(115200);
-//   Serial.println("Hello, testing.");
-// }
+//setup serial communication between arduino and esp8266 in pins 2 and 3
+SoftwareSerial esp8266(2, 3);
+#define DHTTYPE DHT11
+#define DHTPIN 5
+#define pulseLED 7
+#define pulseSensorThreshold 520
+#define distanceThreshold 10
 
-// void loop() {
-//   // use software serial to read data from pins 2 and 3 as tx and rx respectively and write it to serial monitor
-//   if (mySerial.available()) {
-//     Serial.write(mySerial.read());
-//   }
-// }
+PulseSensorPlayground pulseSensor; //create a new instance of the PulseSensorPlayground object
+Ultrasonic ultrasonic1(9, 10); //trig, echo
+DHT dht(DHTPIN, DHTTYPE);
+int distance = 0, pastDistance = 0;
+int heartrate = 0;
+byte samplesUntilReport;
+const byte SAMPLES_PER_SERIAL_SAMPLE = 5;
+void setup(){
+  Serial.begin(9600);
+  esp8266.begin(9600);
+  pinMode(A1, INPUT);
+  pulseSensor.analogInput(0); //set the analog input pin for the PulseSensor
+  pulseSensor.blinkOnPulse(pulseLED); //set the LED pin for the PulseSensor
+  pulseSensor.setThreshold(pulseSensorThreshold); //set the threshold for the PulseSensor
+  if (pulseSensor.begin()) {
+    Serial.println("PulseSensor found!");
+  }
+  else {
+    Serial.println("No PulseSensor found!");
+  }
+  // Skip the first SAMPLES_PER_SERIAL_SAMPLE in the loop().
+  samplesUntilReport = SAMPLES_PER_SERIAL_SAMPLE;
+}
+
+void sensorTasks(){
+  //read ultrasound sensor
+  distance = ultrasonic1.read();
+
+  //read DHT sensor
+  float h = dht.readHumidity();
+  float f = dht.readTemperature(true);
+  //send data to esp8266 depending on threshold and negative values
+  ArduinoJson::StaticJsonDocument<200> doc;
+  if(heartrate > 0){
+    if(heartrate < 200 && heartrate >50){
+      doc["heartrate"] = heartrate;
+    }
+  }
+  if (abs(distance - pastDistance) > distanceThreshold) {
+    doc["distance"] = distance;
+    pastDistance = distance;
+  }
+  if(!isnan(h) && !isnan(f)){
+    doc["temperature"] = f;
+    doc["humidity"] = h;
+  }
+  String json;
+  serializeJson(doc, json);
+  if(json.length() > 0 && json != "null"){
+    esp8266.println(json);
+    Serial.println(json);
+  }
+
+
+}
+void loop(){
+// For debugging, print esp8266 response to serial monitor
+  if (esp8266.available()) {
+    //read until available
+    while (esp8266.available()) {
+      Serial.write(esp8266.read());
+    }
+  }
+  //read heartrate sensor
+  if (pulseSensor.sawNewSample()) {
+    if (--samplesUntilReport == (byte) 0) {
+      samplesUntilReport = SAMPLES_PER_SERIAL_SAMPLE;
+      if (pulseSensor.sawStartOfBeat()) {
+        heartrate = pulseSensor.getBeatsPerMinute();
+      }
+    }
+  }
+  //every 2 seconds print data to esp8266
+  if (millis() % 2000 == 0) {
+    sensorTasks();
+  }
+}
 
 //empty sketch
-void setup(){
+// void setup(){
 
-}
+// }
+// void loop(){
 
-void loop(){
-
-}
+// }
--- ESP01SMesh/src/ordinaryNodes/main.cpp
+++ ESP01SMesh/src/ordinaryNodes/main.cpp
@@ -1,31 +1,36 @@
-// #include "painlessMesh.h"
-// #include <Arduino.h>
+#include "painlessMesh.h"
+#include <Arduino.h>
+#include <ArduinoJson.h>
 
-// #define MESH_PREFIX "whateverYouLike"
-// #define MESH_PASSWORD "somethingSneaky"
-// #define MESH_PORT 5555
+#define MESH_PREFIX "whateverYouLike"
+#define MESH_PASSWORD "somethingSneaky"
+#define MESH_PORT 5555
 
-// // prototypes
-// void receivedCallback( uint32_t from, String &msg );
-// painlessMesh  mesh;
-// String msg = String(mesh.getNodeId());
+// prototypes
+void receivedCallback( uint32_t from, String &msg );
+painlessMesh  mesh;
+String msg = String(mesh.getNodeId());
 
-// void setup() {
-//   Serial.begin(115200);
-//   mesh.setDebugMsgTypes( ERROR | STARTUP | CONNECTION );  // set before init() so that you can see startup messages
-//   mesh.init( MESH_PREFIX, MESH_PASSWORD, MESH_PORT, WIFI_AP_STA, 6 );
-//   // This node and all other nodes should ideally know the mesh contains a root, so call this on all nodes
-//   mesh.setContainsRoot(true);
-// }
+void setup() {
+  Serial.begin(9600);
+  mesh.setDebugMsgTypes( ERROR | STARTUP | CONNECTION );  // set before init() so that you can see startup messages
+  mesh.init( MESH_PREFIX, MESH_PASSWORD, MESH_PORT, WIFI_AP_STA, 6 );
+  // This node and all other nodes should ideally know the mesh contains a root, so call this on all nodes
+  mesh.setContainsRoot(true);
+}
 
-// void loop() {
-//   // every 2 seconds send id of self to root
-//   if (millis() % 2000 == 0) {
-//     //send msg only to root
-//     String msg = String(mesh.getNodeId());
-//     Serial.printf("%s:Sending msg: %s\n", msg.c_str(), msg.c_str());
-//     mesh.sendBroadcast(msg);
-//   }
-//   mesh.update();
-// }
+void loop() {
+  // if nodes>1, and serial input is available, send it to the root
+  if (Serial.available()) {
+    //handle serial input with error handling
+    String input = Serial.readStringUntil('\n');
+    input.trim();
+    if (input.length() > 0) {
+      //send to root
+      mesh.sendBroadcast( input );
+    }
+    Serial.printf("Sending %s to root\n", input.c_str());
+  }
+  mesh.update();
+}
 
